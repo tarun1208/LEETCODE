dbd3f8952dc6e2a3bccb68ae91bc299fcd3db826
NUNNA TARUN SRI VENKATA SAI <160224015+tarun1208@users.noreply.github.com>
2025-07-31 17:40:01 +0530

    Create 3_LONGEST_STRING_WITHOUT_REPLACEMENT.java

---
+++ 3_LONGEST_STRING_WITHOUT_REPLACEMENT.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        String current = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            String ch = String.valueOf(s.charAt(i));
+            if (current.contains(ch)) {
+                int index = current.indexOf(ch);
+                current = current.substring(index + 1);
+            }
+            current += ch;
+            max = Math.max(max, current.length());
+        }
+        return max;
+    }
+}
